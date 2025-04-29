81c02ae0e329a3c69a88934e134032865824e26b
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 17:18:17 +0530

    Update Database connection setup

--- StudentOperations.java
+++ StudentOperations.java
@@ -4,4 +4,8 @@ import java.util.*;
 
 //StudentOperations.java
 public class StudentOperations {
-}
+    //Establishing the connection
+    private static final String URL = "jdbc:mysql://localhost:3306/db_java";
+    private static final String USER = "root";
+    private static final String PASSWORD = "2407";
+
