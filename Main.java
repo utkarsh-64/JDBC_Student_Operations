2d6b16c1fe5b3becec812c42bab34a7926f1af83
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 17:28:37 +0530

    Create Base Application Structure

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
+//Main.java
+//Name: Utkarsh Lakhani
+//PRN: 23070126064
+//Batch: AIML-A3
 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+        }
+    }
+}
